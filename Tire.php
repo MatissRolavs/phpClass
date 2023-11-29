d8300e9b5196de10b104e83ff1089e0abf6ace6e
Skolnieks <skolnieks.pjvt@pjvt.local>
2023-11-29 09:13:03 +0200

    tire boom

---
+++ Tire.php
@@ -0,0 +1,13 @@
+<?php
+
+class Tire{
+
+
+    function __construct(public $size,public $type,private $quality){
+    }
+    function __destruct(){
+        echo "<Br>"."BOOM!";
+    }
+}
+
+?>
--- index.php
+++ index.php
@@ -1,11 +1,15 @@
 <?php
 
-include "Car.php";
+// include "Car.php";
+include "Tire.php";
 
-$car1 = new Car("BMW",170);
-$car2 = new Car("Volvo",69);
-$car1->increaseMileage(100);
-Car::$describe;
-Car::makeNoise();
-echo "<br>";
+// $car1 = new Car("BMW",170);
+// $car2 = new Car("Volvo",69);
+// $car1->increaseMileage(100);
+// Car::$describe;
+// Car::makeNoise();
+// echo "<br>";
+
+$tire1 = new Tire("69inches", "winter","good");
+echo $tire1->size." ".$tire1->type;
 ?>
